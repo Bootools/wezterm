f036095c85ebf5a943372500012342fff9db5bd5
Blind Tear <blindtear@blindtear.com>
2024-10-12 20:53:51 -0400

    add tab bar and configure integrated icons

--- wezterm.lua
+++ wezterm.lua
@@ -4,14 +4,41 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- This will hold the Powerline symbols used in the integrated tab bar buttons
+local CLOSE_BUTTON = wezterm.nerdfonts.md_window_close
+local MAXIMIZE_BUTTON = wezterm.nerdfonts.md_window_maximize
+local MINIMIZE_BUTTON = wezterm.nerdfonts.md_window_minimize
+
 -- This is where you actually apply your config choices
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 16.0
 config.default_cursor_style = 'SteadyBar'
 config.cursor_thickness = '1.4pt'
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+
+config.tab_bar_style = {
+    window_close = wezterm.format {
+        { Text = ' ' .. CLOSE_BUTTON .. ' ' }, 
+    },
+    window_close_hover = wezterm.format {
+        { Text = ' ' .. CLOSE_BUTTON .. ' ' }, 
+    },
+    window_maximize = wezterm.format {
+        { Text = ' ' .. MAXIMIZE_BUTTON .. ' ' },
+    },
+    window_maximize_hover = wezterm.format {
+        { Text = ' ' .. MAXIMIZE_BUTTON .. ' ' },
+    },
+    window_hide = wezterm.format {
+        { Text = ' ' .. MINIMIZE_BUTTON .. ' ' },
+    },
+    window_hide_hover = wezterm.format {
+        { Text = ' ' .. MINIMIZE_BUTTON .. ' ' },
+    },
+}
+
+config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
