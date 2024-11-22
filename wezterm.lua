3658777ecef32725f018598195897d66afdc1426
Blind Tear <blindtear@blindtear.com>
2024-11-21 19:23:37 -0500

    remove padding from window

--- wezterm.lua
+++ wezterm.lua
@@ -41,6 +41,12 @@ config.tab_bar_style = {
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 -- and finally, return the configuration to wezterm
 return config
